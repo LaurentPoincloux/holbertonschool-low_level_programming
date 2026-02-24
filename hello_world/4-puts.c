a6a4da53709fff43359d46e5643c6d9a71f1ee72
LaurentPoincloux <laurent.poincloux@holbertonstudents.com>
2026-02-24 08:38:50 +0000

    Add Task 4 v2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
+ * Description: prints a specific sentence using puts
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
