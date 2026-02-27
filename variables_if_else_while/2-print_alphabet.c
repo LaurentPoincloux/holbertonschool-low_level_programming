6c4c1ad42af8a59dd4af62bde79e246081e7d2f9
Laurent Poincloux <laurent.poincloux@holbertonstudents.com>
2026-02-27 08:56:29 +0000

    Add Task 2 v2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: Prints the alphabet in lowercase, followed by a new line
- * 
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Description: Prints the alphabet in lowercase, followed by a new line
+* 
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-  char alphabet = 'a';
+char alphabet = 'a';
 
- while (alphabet <='z')
+while (alphabet <= 'z')
 {
-  putchar(alphabet);
-  alphabet++;
- }  
+putchar(alphabet);
+alphabet++;
+}  
 
- putchar('\n');
-   return (0);
+putchar('\n');
+return (0);
 }
