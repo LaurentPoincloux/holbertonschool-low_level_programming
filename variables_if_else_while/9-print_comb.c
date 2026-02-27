88f583214a26d1c821453541f97d00ba0f856864
Laurent Poincloux <laurent.poincloux@holbertonstudents.com>
2026-02-27 12:55:22 +0000

    Add Task 9 v1

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Prints all the single-digit numbers separated by a coma
+* followed by a space
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int n = 0;
+
+while (n <= 9)
+{
+putchar (n  + '0');
+ if (n != 9)
+{
+putchar(',');
+putchar(' ');
+}
+n++;
+}
+
+putchar('\n');
+return (0);
+}
