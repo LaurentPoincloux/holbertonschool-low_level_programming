ac71e31e49a5ffa9603b16a854b009324346c3a8
Laurent Poincloux <laurent.poincloux@holbertonstudents.com>
2026-02-27 12:08:17 +0000

    Add Task 7 v1

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -3,7 +3,8 @@
 /**
 * main - Entry point
 *
-* Description: Prints the alphabet in lowercase, followed by a new line
+* Description: Prints the alphabet in reverse
+* followed by a new line
 *
 * Return: Always 0 (Success)
 */
