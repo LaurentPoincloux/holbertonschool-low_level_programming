a8c6ec74beca78fd46a963d8c852566433dc1f4a
Laurent Poincloux <laurent.poincloux@holbertonstudents.com>
2026-02-27 09:29:59 +0000

    Add Task 3 v1

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Prints tha laphabet in lowercase then uppercase,
+* followed by a new line
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char lower = 'a';
+char upper = 'A';
+
+while (lower <= 'z')
+{
+putchar(lower);
+lower++;
+}
+
+while (upper <= 'Z')
+{
+putchar(upper);
+upper++;
+}
+
+putchar('\n');
+return (0);
+}
