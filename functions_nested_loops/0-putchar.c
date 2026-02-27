219f3d05963bd1744866194d111801d6f082ba7b
Laurent Poincloux <laurent.poincloux@holbertonstudents.com>
2026-02-27 13:57:22 +0000

    Add Task 0 v1

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Prints all the single-digit numbers separated by a coma
+* followed by a space
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+
+return (0);
+}
